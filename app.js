63156a9d64e113be6c4afb5c6a99450155141f1a
HAMALAWY\Lenovo <te7a.12345@gmail.com>
2024-08-13 22:31:09 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// package Imports
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config/config");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+const port = config.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
