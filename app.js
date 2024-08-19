bc834d68c6ff967f94e927c72d5995c5fe4ccced
HAMALAWY\Lenovo <te7a.12345@gmail.com>
2024-08-19 22:27:03 +0300

    Adding Application Models

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-// package Imports
+// packages and File  Imports
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./config/config");
 const connectDB = require("./config/db");
 
+// Run Database and Express
+// TODO: Add Database ASYNC
 connectDB();
-
 const app = express();
 const port = config.port || 3000;
 
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//  Start Server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
